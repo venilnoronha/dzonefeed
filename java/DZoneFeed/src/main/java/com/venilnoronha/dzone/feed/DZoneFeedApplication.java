6047b598232b0eae96444cfca81a99a92c3c1861
Venil <Venil@WINDOWSXP.local>
2015-08-28 10:01:10 +0530

    Initial commit.

---
+++ java/DZoneFeed/src/main/java/com/venilnoronha/dzone/feed/DZoneFeedApplication.java
@@ -0,0 +1,12 @@
+package com.venilnoronha.dzone.feed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DZoneFeedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DZoneFeedApplication.class, args);
+    }
+}
