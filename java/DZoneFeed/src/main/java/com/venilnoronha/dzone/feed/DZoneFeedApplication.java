57ff319dea43d4e1a67b176baf3e6d11354c0e79
Venil Noronha <venil.noronha@gmail.com>
2015-08-30 12:04:23 +0530

    Default TimeZone UTC.

--- java/DZoneFeed/src/main/java/com/venilnoronha/dzone/feed/DZoneFeedApplication.java
+++ java/DZoneFeed/src/main/java/com/venilnoronha/dzone/feed/DZoneFeedApplication.java
@@ -1,5 +1,7 @@
 package com.venilnoronha.dzone.feed;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +9,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DZoneFeedApplication {
 
     public static void main(String[] args) {
+    	TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
         SpringApplication.run(DZoneFeedApplication.class, args);
     }
+    
 }
